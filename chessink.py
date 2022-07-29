882a0dde0d01154ee77f46a8e5e97c3208693b77
Pablo Bustos <pablo@groupon.com>
2022-07-29 17:02:40 -0500

    process_button draft

--- chessink.py
+++ chessink.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+
+from threading import Lock
+
 import sys
 import os
 picdir = os.path.join(os.environ["WAVESHARE_DIR"], 'pic')
 libdir = os.path.join(os.environ["WAVESHARE_DIR"], 'lib')
 sys.path.append(libdir)
+
+from gpiozero import Button
 
 import logging
 from waveshare_epd import epd1in54_V2
@@ -13,6 +18,50 @@ from PIL import Image,ImageDraw,ImageFont
 import traceback
 
 logging.basicConfig(level=logging.DEBUG)
+
+msg = '____'
+msgt = msg
+msgi = 0
+mutex = Lock()
+def on_button_a_pressed():
+    on_button_pressed('a')
+def on_button_b_pressed():
+    on_button_pressed('b')
+def on_button_c_pressed():
+    on_button_pressed('c')
+
+def on_button_pressed(id):
+    global mutex
+    if mutex.acquire(False):
+        global msg
+        global msgi
+        msg, msgi = process_button(msg, msgi, id)
+        time.sleep(0.15) # "pressed" triggers multiple events for more than 0.1s
+        mutex.release()
+
+def process_button(msg, msgi, button):
+    aux = list(msg)
+    if button == 'a':
+        if aux[msgi] == '_':
+            aux[msgi] = 'a' if msgi % 2 == 0 else '1'
+        else:
+            aux[msgi] = chr(ord(aux[msgi]) + 1)
+    if button == 'b':
+        if aux[msgi] == '_':
+            aux[msgi] = 'd' if msgi % 2 == 0 else '4'
+        else:
+            aux[msgi] = chr(ord(aux[msgi]) + 4)
+    if button == 'c':
+        msgi += 1
+    return ''.join(aux), msgi
+
+def changed():
+    global msg
+    global msgt
+    res = (msg != msgt)
+    if res:
+        msgt = msg
+    return res
 
 try:
     logging.info("epd1in54_V2 Demo")
@@ -32,15 +81,20 @@ try:
     time_draw = ImageDraw.Draw(time_image)
 
     epd.init(1) # into partial refresh mode
-    num = 0
+    button_a = Button(13)
+    button_b = Button(19)
+    button_c = Button(26)
+    button_a.when_pressed = on_button_a_pressed
+    button_b.when_pressed = on_button_b_pressed
+    button_c.when_pressed = on_button_c_pressed
     while (True):
-        time_draw.rectangle((10, 10, 120, 50), fill = 255)
-        time_draw.text((10, 10), time.strftime('%H:%M:%S'), font = font, fill = 0)
-        epd.displayPart(epd.getbuffer(time_image))
-        num = num + 1
-        if(num == 5):
-            break
-        time.sleep(1)
+        if changed():
+            time_draw.rectangle((10, 10, 120, 50), fill = 255)
+            time_draw.text((10, 10), msg, font = font, fill = 0)
+            epd.displayPart(epd.getbuffer(time_image))
+        else:
+            logging.info("no changes, sleep...")
+            time.sleep(1)
 
     logging.info("Clear...")
     epd.init(0)
