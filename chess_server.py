1fae7a6b73ec724e5f75590d5db7ece38bf95cd4
Pablo Bustos <pablo@groupon.com>
2022-08-04 12:26:07 -0500

    mvp

--- chess_server.py
+++ chess_server.py
@@ -1,7 +1,7 @@
 import chess
 import chess.engine
 
-# curl -X POST -d '{"move": "d2d4", "max_ms": "0.1", "board": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"}' localhost:9000
+# curl -X POST -d '{"move": "d2d4", "max_time_s": 0.1, "board": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"}' localhost:9000
 # {"board": "rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq - 0 1", "move": "g8f6"}
 
 class ChessInkServer:
@@ -13,9 +13,10 @@ class ChessInkServer:
         self.board = chess.Board(board_fen)
         move = self.board.parse_uci(move_uci)
         self.board.push(move)
-    def play(self, max_ms):
-        engine = chess.engine.SimpleEngine.popen_uci(r"/root/Stockfish-sf_15/src/stockfish")
-        result = engine.play(self.board, chess.engine.Limit(time=max_ms))
+    def play(self, max_time_s):
+        engine = chess.engine.SimpleEngine.popen_uci(r"/home/pi/utils/stockfish")
+        # engine = chess.engine.SimpleEngine.popen_uci(r"/root/Stockfish-sf_15/src/stockfish")
+        result = engine.play(self.board, chess.engine.Limit(time=max_time_s))
         engine.quit()
         self.board.push(result.move)
         return result.move.uci()
@@ -29,23 +30,23 @@ hostPort = 9000
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         cs = ChessInkServer()
-        game = {'board': cs.get_board_fen()}
+        res_data = {'board': cs.get_board_fen()}
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        self.wfile.write(bytes(json.dumps(game)+"\n", "utf-8"))
+        self.wfile.write(bytes(json.dumps(res_data)+"\n", "utf-8"))
     def do_POST(self):
         try:
             content = self.rfile.read(int(self.headers['Content-Length']))
-            game = json.loads(content.decode("utf-8"))
+            req_data = json.loads(content.decode("utf-8"))
             cs = ChessInkServer()
-            cs.set_board(game['board'], game['move'])
-            move = cs.play(float(game['max_ms']))
-            game = {'board': cs.get_board_fen(), 'move': move}
+            cs.set_board(req_data['board'], req_data['move'])
+            move = cs.play(req_data['max_time_s'])
+            res_data = {'board': cs.get_board_fen(), 'move': move}
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(bytes(json.dumps(game)+"\n", "utf-8"))
+            self.wfile.write(bytes(json.dumps(res_data)+"\n", "utf-8"))
         except Exception as e:
             self.send_response(500)
             self.end_headers()
--- chessink.py
+++ chessink.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
+CS_URL = 'http://localhost:9000'
+
 from threading import Lock
 
 import sys
@@ -19,10 +21,22 @@ import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
-move = [0,0,0,0]
-movet = None
-movei = 0
-mutex = Lock()
+import urllib.request
+import json
+def cs_start_board():
+    with urllib.request.urlopen(CS_URL) as response:
+        body = response.read()
+        res_data = json.loads(body)
+        return res_data['board']
+def cs_play(board_fen, move):
+    req_data = {'board': board_fen, 'move': move, 'max_time_s': 1.0 }
+    req =  urllib.request.Request(CS_URL, data=json.dumps(req_data).encode('utf-8'))
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        res_data = json.loads(body)
+        board_fen = res_data['board']
+        return board_fen, res_data['move']
+
 def on_button_a_pressed():
     on_button_pressed('a')
 def on_button_b_pressed():
@@ -48,7 +62,6 @@ def process_button_move(button):
 def changed():
     global move
     global movet
-    logging.info(f"hello5 {move} {movet}")
     res = (move != movet)
     if res:
         movet = move.copy()
@@ -67,49 +80,64 @@ def move2str(move): # e.g. 4244 -> d2d4
                 aux[i] = x
     return ''.join(str(x) for x in aux)
 
+
+move = [0,0,0,0]
+movet = None
+movei = 0
+mutex = Lock()
+board_fen = cs_start_board()
+
+button_a = Button(13)
+button_b = Button(19)
+button_c = Button(26)
+button_a.when_pressed = on_button_a_pressed
+button_b.when_pressed = on_button_b_pressed
+button_c.when_pressed = on_button_c_pressed
+
 try:
-    logging.info("epd1in54_V2 Demo")
+    logging.info("init epd1in54_V2")
     
     epd = epd1in54_V2.EPD()
-    
-    logging.info("init and Clear")
     epd.init(0)
     epd.Clear(0xFF)
     
     # partial update
-    logging.info("4.show time...")
+    logging.info("displayPartBaseImage")
     font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    time_image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    epd.displayPartBaseImage(epd.getbuffer(time_image))
+    chess_image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    epd.displayPartBaseImage(epd.getbuffer(chess_image))
     
-    time_draw = ImageDraw.Draw(time_image)
+    chess_draw = ImageDraw.Draw(chess_image)
 
     epd.init(1) # into partial refresh mode
-    button_a = Button(13)
-    button_b = Button(19)
-    button_c = Button(26)
-    button_a.when_pressed = on_button_a_pressed
-    button_b.when_pressed = on_button_b_pressed
-    button_c.when_pressed = on_button_c_pressed
     while (True):
         if movei == 4:
-            logging.info("move: %s" % move)
-            move = [0,0,0,0]
-            movei = 0
+            logging.info(f"black move: {move2str(move)}")
+            try:
+                board_fen, cs_move = cs_play(board_fen, move2str(move))
+                move = [0,0,0,0]
+                movei = 0
+                chess_draw.rectangle((10, 50, 120, 100), fill = 255)
+                chess_draw.text((10, 50), f"bk: {cs_move}", font = font, fill = 0)
+                epd.displayPart(epd.getbuffer(chess_image))
+            except Exception as e:
+                move = [0,0,0,0]
+                movei = 0
+                logging.error(e)
         if changed():
             logging.info("drawing changes")
-            time_draw.rectangle((10, 10, 120, 50), fill = 255)
-            time_draw.text((10, 10), move2str(move), font = font, fill = 0)
-            epd.displayPart(epd.getbuffer(time_image))
+            chess_draw.rectangle((10, 10, 120, 50), fill = 255)
+            chess_draw.text((10, 10), f"wt: {move2str(move)}", font = font, fill = 0)
+            epd.displayPart(epd.getbuffer(chess_image))
         else:
             logging.info("no changes, sleep...")
             time.sleep(1)
 
-    logging.info("Clear...")
+    logging.info("clear...")
     epd.init(0)
     epd.Clear(0xFF)
 
-    logging.info("Goto Sleep...")
+    logging.info("sleep...")
     epd.sleep()
 
 except IOError as e:
